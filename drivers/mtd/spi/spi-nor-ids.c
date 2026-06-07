396a8c201096286985181f5dd589a355c8031a75
gtxaspec <gtxent@gmail.com>
2026-06-06 19:01:42 -0700

    mtd: spi-nor: add Fidelix FM25Q64
    Add the Fidelix FM25Q64 (8 MiB, mfr id 0xf8). Verified against flashrom (FIDELIX_FM25Q64 0x3217) and the Fidelix FM25Q64 datasheet. Distinct from the same-named Fudan FM25Q64 at 0xa14017.

--- drivers/mtd/spi/spi-nor-ids.c
+++ drivers/mtd/spi/spi-nor-ids.c
@@ -121,6 +121,11 @@ const struct flash_info spi_nor_ids[] = {
 		.addr_width = 2,
 	},
 #endif
+#ifdef CONFIG_SPI_FLASH_FIDELIX
+	/* Fidelix */
+	{ INFO("fm25q64",    0xf83217, 0, 64 * 1024, 128,
+	       SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
+#endif
 #ifdef CONFIG_SPI_FLASH_GIGADEVICE	/* GIGADEVICE */
 	/* GigaDevice */
 	{
